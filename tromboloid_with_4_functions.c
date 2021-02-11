a29a4e4ddb0fedda1a22aa909961739ee1cddfd7
Harshal Goyal <53938512+HarshalGoyal@users.noreply.github.com>
2021-02-11 10:18:56 +0530

    Updated vi call by refrence

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -2,41 +2,39 @@
 
 #include<stdio.h>
 
-float read_parameters();
-float calc_vol(float,float,float);
-int display(float);
+float read_parameter();
+float calc_vol(float *,float *,float *);
+int display(float *);
 
-float read_parameters()
+
+float read_parameter()
 {
-    float h,d,b,vol;
-    printf("\n Enter the value of h:");
-    scanf("%f",&h);
-    printf("\n Enter the value of d:");
-    scanf("%f",&d);
-    printf("\n Enter the value of b:");
-    scanf("%f",&b);
-
-    vol = calc_vol(h,d,b);
-    display(vol);
-    return 0.0;
+    float var;
+    printf("\nEnter the value: ");
+    scanf("%f",&var);
 }
 
-float calc_vol(float h, float d, float b)
+float calc_vol(float* h, float* d, float* b)
 {
     float vol = 0.0;
-    vol = (1.0/3.0) * ((h*d*b)+(d/b));
+    vol = (1.0/3.0) * ((*h * (*d) * (*b))+((*d)/ (*b)));
     return vol;
 }
 
-int display(float vol)
+int display(float* vol)
 {
-    printf("\n Volume of Tromboloid is: %.2f \n",vol);
+    printf("\n Volume of Tromboloid is: %.2f \n", *vol);
     return 0;
 }
 
-
 int main()
 {
-    read_parameters();   
+    float h,b,d,vol;
+    h=read_parameter();
+    d=read_parameter();
+    b=read_parameter();
+    vol = calc_vol(&h,&d,&b);
+    display(&vol);
+
     return 0;
 }
