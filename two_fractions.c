53ed0e4ad646ffdd5146c722358b321771365963
Harshal Goyal <53938512+HarshalGoyal@users.noreply.github.com>
2021-02-17 09:49:23 +0530

    fixed

--- two_fractions.c
+++ two_fractions.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 struct fraction{
-    float num, den;
+    int num, den;
 };
 
 int gcd(int a, int b) 
@@ -13,7 +13,7 @@ int gcd(int a, int b)
     return gcd(b%a, a); 
 } 
 
-void lowest(float den3, float num3) 
+void lowest(int den3, int num3) 
 { 
 
     int common_factor = gcd(num3,den3); 
@@ -25,20 +25,20 @@ struct fraction get_prams()
 {
     struct fraction p1;
     printf("\nEnter the fraction(num,den): ");
-    scanf("%f %f",&p1.num,&p1.den);
+    scanf("%d %d",&p1.num,&p1.den);
     return p1;
 }
 
 
 int main(){
     struct fraction c1,c2;
-    float num3,den3;
+    int num3,den3;
     c1=get_prams();
     c2=get_prams();
     den3=gcd(c1.den,c2.den);
     den3 = (c1.den*c2.den) / den3;
     num3 = (c1.num)*(den3/c1.den) + (c2.num)*(den3/c2.den);
     lowest(den3,num3);
-    printf("\nResultatnt: %f / %f \n",num3,den3);
+    printf("\nResultatnt: %d / %d \n",num3,den3);
     return 0;
 }
