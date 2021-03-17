a3e88fe2d3d2970921f684c626440275a81bf5f6
Harshal Goyal <53938512+HarshalGoyal@users.noreply.github.com>
2021-03-17 14:06:40 +0530

    using structs

--- complex_numbers_sum.c
+++ complex_numbers_sum.c
@@ -1,71 +1,110 @@
 //WAP to find the sum of n complex numbers using structures and 4 or more functions.
 
-#include <stdio.h>
+#include<stdio.h>
 
-int input( int size, int num[], int den[])
+struct fract
 {
-    int i;
-    
-    for(i=0;i<size;i++)
+    int num,den;
+};
+
+
+typedef struct fract fraction;
+
+
+int gcd(int a, int b) 
+{ 
+    if (b != 0)
+        return gcd(b, a % b);
+    else
+        return a;
+} 
+
+int lcm(fraction frac[],int n)
+{
+    int ans = frac[0].den;
+
+    for(int i=1;i<n;i++)
     {
-    	printf("\nEnter the numerator: ");
-   	scanf("%d",&num[i]);
-    	printf("\nEnter the denominator: ");
-    	scanf("%d",&den[i]);
+        ans = (((frac[i].den * ans))/(gcd(frac[i].den,ans)));
     }
+
+    return ans;
 }
 
-int lcm(int a, int b)
+fraction input_one(fraction frac)
 {
-    int m = 1;
- 
-    while(m%a || m%b) m++;
- 
-    return m;
+    printf("\n enter the fraction: ");
+    scanf("%d %d",&frac.num,&frac.den);
+    return frac;
 }
 
-int compute(int size,int den[],int num[], int *final_num, int *final_den)
+fraction compute_two(fraction frac1, fraction frac2)
 {
+    fraction temp;
+    int lcm_den =(frac1.den,frac2.den);
+    temp.num = (frac1.num * (gcd(frac1.num,lcm_den)))+(frac2.num * (gcd(frac2.num,lcm_den)));
+    temp.den = lcm_den;
+    return temp;
+}
 
-//final_den = 0;
-int tempNum = 0, tempDen = 0;
-tempDen  = den[0];
-
-int i;
-
-for (i = 1; i < size; i++)
+fraction display_one(fraction frac)
 {
-    tempDen = lcm(tempDen, den[i]);
+    printf("\n Sum of two fractions is: %d / %d \n",frac.num,frac.den);
 }
 
-	for(i=0;i<size;i++)
-	{	
-		tempNum += (tempDen / den[i]) * num[i];
-	}
-*final_num = tempNum;
-*final_den = tempDen;
-}
-
-int output(int final_num,int final_den)
+fraction input_n(fraction fracs[] , int n)
 {
-    printf("\nThe added fraction is %d/%d ",final_num,final_den);
-    printf("\n");
+    for(int i=0;i<n;i++)
+    {
+        fracs[i]=input_one(fracs[i]);
+    }
+
+    return fracs[n];
+
 }
 
+fraction compute_n(fraction fracs[],int n)
+{
+    fraction temp;
+    int num_temp,reduced_den;
+    temp.den = lcm(fracs,n);
+    temp.num = 0;
+    for(int i=0;i<n;i++)
+    {
+
+        num_temp = gcd(fracs[i].den,temp.den);
+
+
+        if(temp.den == fracs[i].den)
+        {
+            temp.num += fracs[i].num * 1;
+        }
+        
+        else
+        {
+            temp.num += fracs[i].num * num_temp;
+        }
+    }
+
+    
+    return temp;
+}
+
+
+int display_res(fraction final_frac)
+{
+    printf("\n Sum of n fractions in lowest form  : %d / %d \n",final_frac.num,final_frac.den);
+    return 0;
+}
 int main()
 {
-    int size,final_num,final_den,i;
-
-    printf("\n Enter the number of fractions: ");
-    scanf("%d",&size);
-    int num[size], den[size];
-
-    input(size,num,den);
-	
-  
-    compute(size,den,num,&final_num, &final_den);
-
-   output(final_num,final_den);
-
+    int n;
+    printf("\n Enter the number of fraction: ");
+    scanf("%d",&n);
+    fraction fracs[n];
+    fracs[n] = input_n(fracs,n);
+    fraction final_frac;
+    final_frac = compute_n(fracs,n);
+    display_res(final_frac);
     return 0;
 }
